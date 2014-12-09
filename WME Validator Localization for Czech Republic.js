912fc82a7f59788bb0aa531948febf40e11e9801
Jan Urbánek <git@zirland.org>
2014-12-09 14:40:04 +0100

    1.1.4: #170 promenáda, zahrada

--- WME Validator Localization for Czech Republic.js
+++ WME Validator Localization for Czech Republic.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                WME Validator Localization for Czech Republic
-// @version             1.0.14
+// @version             1.1.4
 // @description         This script localizes WME Validator for Czech Republic. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
 // @match               https://www.waze.com/*editor/*
@@ -41,7 +41,7 @@ window.WME_Validator_Czech_Republic = {
   ".country": "Czech Republic",
   ".codeISO": "CZ",
   ".author": "Kebb01, petrjanik, Zirland, pvo11",
-  ".updated": "2014-11-22",
+  ".updated": "2014-12-09",
   ".link": "https://greasyfork.org/scripts/2972-wme-validator-localization-for-czech-republic",
   ".lng": "CS",
   "city.consider.en": "consider this city name:",
@@ -935,7 +935,7 @@ window.WME_Validator_Czech_Republic = {
   "154.solution": "Uzamkněte segment",
   "170.enabled": true,
   "170.params": {
-    "regexp": "/^(?!(náměstí|nám\\.|nábřeží|nábř\\.|třída|tř\\.|tunel|most|ulice|ul\\.|sídl\\.|sídliště|park|sad|sady|gen\\.|plk\\.|pplk\\.|mjr\\.|kpt\\.|por\\.|dr\\.|bratří|bratranců) [^a-z])[a-z]/"
+    "regexp": "/^(?!(náměstí|nám\\.|nábřeží|nábř\\.|třída|tř\\.|tunel|most|ulice|ul\\.|sídl\\.|sídliště|park|sad|sady|gen\\.|plk\\.|pplk\\.|mjr\\.|kpt\\.|por\\.|dr\\.|bratří|bratranců|promenáda|zahrada) [^a-z])[a-z]/"
   },
   "170.title.en": "Lowercase street name",
   "170.title": "Název ulice malým písmenem",
