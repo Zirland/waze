121d5af3d4c130a7810c552206bcb1a284d465e7
Jan Urbánek <git@zirland.org>
2015-12-01 14:58:56 +0100

    1.1.12: #154 enabled

--- L4 WME Validator Localization for Czech Republic.js
+++ L4 WME Validator Localization for Czech Republic.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                L4 WME Validator Localization for Czech Republic
-// @version             1.1.11
+// @version             1.1.12
 // @description         This script localizes WME Validator for Czech Republic. You also need main package (WME Validator) installed.
 // @match               https://editor-beta.waze.com/*editor/*
 // @match               https://www.waze.com/*editor/*
@@ -41,7 +41,7 @@ window.WME_Validator_Czech_Republic = {
   ".country": "Czech Republic",
   ".codeISO": "CZ",
   ".author": "Kebb01, petrjanik, Zirland, pvo11",
-  ".updated": "2015-09-10",
+  ".updated": "2015-12-01",
   ".link": "https://greasyfork.org/cs/scripts/8476-l4-wme-validator-localization-for-czech-republic",
   ".lng": "CS",
   "city.consider.en": "consider this city name:",
@@ -936,7 +936,7 @@ window.WME_Validator_Czech_Republic = {
   "153.problem": "Segment rampy by měl být uzamčen alespoň na úroveň ${n}",
   "153.solution.en": "Lock the segment",
   "153.solution": "Uzamkněte segment",
-  "154.enabled": false,
+  "154.enabled": true,
   "154.params": {
     "n": 2
   },
